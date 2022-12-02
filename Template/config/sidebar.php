6e407b5f73f8eba891843919c048761aa1de7119
aljawaid <jawaid_imran@hotmail.com>
2022-12-02 18:11:03 +0000

    Fix Active Menu Link
    - Fix title also

--- Template/config/sidebar.php
+++ Template/config/sidebar.php
@@ -1,3 +1,3 @@
-<li<?= $this->app->checkMenuSelection('MatomoAnalytics', 'show', 'MatomoAnalytics') ?>>
-    <?= $this->url->link(t('Matomo Analytics'), 'MatomoAnalyticsController', 'show', ['plugin' => 'MatomoAnalytics']) ?>
+<li <?= $this->app->checkMenuSelection('MatomoAnalyticsController', 'show', 'MatomoAnalytics') ?>>
+    <?= $this->url->link(t('MatomoAnalytics'), 'MatomoAnalyticsController', 'show', ['plugin' => 'MatomoAnalytics']) ?>
 </li>
