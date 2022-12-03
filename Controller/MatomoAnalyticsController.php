a821a0dbd2e0515b0f3cbd1d9cc128f55618b503
aljawaid <jawaid_imran@hotmail.com>
2022-12-03 04:11:18 +0000

    Add Form Save Settings Function
    - Needed this to go to the correct redirect url

--- Controller/MatomoAnalyticsController.php
+++ Controller/MatomoAnalyticsController.php
@@ -26,4 +26,23 @@ class MatomoAnalyticsController extends \Kanboard\Controller\ConfigController
             'title' => 'MatomoAnalytics &#10562; '.t('Settings'),
         )));
     }
+
+    /**
+     * Save settings
+     *
+     */
+    public function save()
+    {
+        $values =  $this->request->getValues();
+        $redirect = $this->request->getStringParam('redirect', 'matomo');
+
+        if ($this->configModel->save($values)) {
+            $this->languageModel->loadCurrentLanguage();
+            $this->flash->success(t('Settings saved successfully.'));
+        } else {
+            $this->flash->failure(t('Unable to save your settings.'));
+        }
+
+        $this->response->redirect($this->helper->url->to('MatomoAnalyticsController', $redirect, ['plugin' => 'MatomoAnalytics']));
+    }
 }
