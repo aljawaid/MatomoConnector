b4967b4fe7e0dbb99b1ba9a1e79f7bbe5de9d9b7
aljawaid <jawaid_imran@hotmail.com>
2022-12-03 05:48:21 +0000

    Add Image Tracker & Fallback Code

--- Template/layout/bottom.php
+++ Template/layout/bottom.php
@@ -1,2 +1,37 @@
-<!-- Matomo Image Tracker-->
-<!-- End Matomo -->
+<?php
+$website_page_name = str_replace(' ', '+', $this->task->configModel->get('website_page_name', ''))
+?>
+
+<!-- MATOMO IMAGE TRACKER -->
+<?php if ($this->task->configModel->get('image_tracking', '') == 'enable_image_tracking'): ?>
+    <?php if (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('website_page_name'))): ?>
+        <img
+        referrerpolicy="no-referrer-when-downgrade"
+        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name ?>" style="border:0" alt="" />
+    <?php elseif (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id'))): ?>
+        <img
+        referrerpolicy="no-referrer-when-downgrade"
+        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1" style="border:0" alt="" />
+    <?php endif ?>
+<!-- END OF MATOMO CODE -->
+<?php endif ?>
+
+<?php if ($this->task->configModel->get('image_tracking', '') == 'enable_image_tracking_js'): ?>
+    <?php if (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('website_page_name'))): ?>
+        <noscript><img
+        referrerpolicy="no-referrer-when-downgrade"
+        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name ?>" style="border:0" alt="" /></noscript>
+    <?php elseif (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id'))): ?>
+        <noscript><img
+        referrerpolicy="no-referrer-when-downgrade"
+        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1" style="border:0" alt="" /></noscript>
+    <?php endif ?>
+<!-- END OF MATOMO CODE -->
+<?php endif ?>
+
+<?php if ($this->task->configModel->get('image_tracking', '') == 'disable_image_tracking'): ?>
+    <!-- MATOMO IMAGE TRACKING DISABLED -->
+<!-- END OF MATOMO CODE -->
+<?php endif ?>
+
+<?php // question mark ? = &#63; // slash / = &#47; // must be no whitespaces in the string ?>
