eef6e3698f67e418bd15e9fbef6ba65ddb696c20
aljawaid <jawaid_imran@hotmail.com>
2022-12-04 12:23:00 +0000

    Move JS Fallback Image Tracking to Head
    - As per Matomo Script guidelines

--- Template/layout/bottom.php
+++ Template/layout/bottom.php
@@ -16,21 +16,8 @@ $website_page_name = str_replace(' ', '+', $this->task->configModel->get('websit
 <!-- END OF MATOMO CODE -->
 <?php endif ?>
 
-<?php if ($this->task->configModel->get('image_tracking', '') == 'enable_image_tracking_js'): ?>
-    <?php if (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('website_page_name'))): ?>
-        <noscript><img
-        referrerpolicy="no-referrer-when-downgrade"
-        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name ?>" style="border:0" alt="" /></noscript>
-    <?php elseif (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id'))): ?>
-        <noscript><img
-        referrerpolicy="no-referrer-when-downgrade"
-        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1" style="border:0" alt="" /></noscript>
-    <?php endif ?>
-<!-- END OF MATOMO CODE -->
-<?php endif ?>
-
 <?php if ($this->task->configModel->get('image_tracking', '') == 'disable_image_tracking'): ?>
-    <!-- MATOMO IMAGE TRACKING DISABLED -->
+    <!-- IMAGE TRACKING DISABLED -->
 <!-- END OF MATOMO CODE -->
 <?php endif ?>
 
