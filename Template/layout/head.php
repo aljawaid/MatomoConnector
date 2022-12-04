84121a56f5adf75cbb128ced8685313b8ce4deb1
aljawaid <jawaid_imran@hotmail.com>
2022-12-04 12:46:34 +0000

    Change variable

--- Template/layout/head.php
+++ Template/layout/head.php
@@ -1,12 +1,16 @@
+<?php
+$website_page_name_js = str_replace(' ', '+', $this->task->configModel->get('website_page_name', ''))
+?>
+
 <!-- Matomo -->
 <!-- End Matomo Code -->
 
-<!-- MATOMO IMAGE TRACKER -->
 <?php if ($this->task->configModel->get('image_tracking', '') == 'enable_image_tracking_js'): ?>
+    <!-- MATOMO IMAGE TRACKER -->
     <?php if (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('website_page_name'))): ?>
         <noscript><p><img
         referrerpolicy="no-referrer-when-downgrade"
-        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name ?>" style="border:0" alt="" /></p></noscript>
+        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name_js ?>" style="border:0" alt="" /></p></noscript>
     <?php elseif (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id'))): ?>
         <noscript><p><img
         referrerpolicy="no-referrer-when-downgrade"
@@ -16,6 +20,7 @@
 <?php endif ?>
 
 <?php if ($this->task->configModel->get('image_tracking', '') == 'disable_image_tracking'): ?>
+<!-- MATOMO IMAGE TRACKER -->
     <!-- IMAGE TRACKING DISABLED -->
 <!-- END OF MATOMO CODE -->
 <?php endif ?>
