25fa5e4d60bc7898ce633907d080f9184e566b53
aljawaid <jawaid_imran@hotmail.com>
2022-12-04 09:39:47 +0000

    Update Integration Bar

--- Template/config/integrations.php
+++ Template/config/integrations.php
@@ -1,6 +1,22 @@
-<div class="panel matomo-connector">
-    <a href="<?= $this->url->href('MatomoConnectorController', 'show', array('plugin' => 'MatomoConnector')) ?>" class="btn" title="<?= t('View Settings') ?>">
-        <img src="/plugins/MatomoConnector/Assets/matomo-logo.svg" alt="<?= t('Matomo icon') ?>">
+<div class="panel matomo-connector page-margin">
+    <h4 class="">
+        Matomo Analytics <?= t('Connector Installed') ?>
+    </h4>
+    <div class="matomo-buttons">
+        <a href="<?= $this->url->href('MatomoConnectorController', 'show', array('plugin' => 'MatomoConnector')) ?>" class="btn" title="">
+        <img src="/plugins/MatomoConnector/Assets/matomo-icon.svg" alt="<?= t('Matomo icon') ?>">
+        <?= t('View Settings') ?>
     </a>
-    <?= t('Connector Installed') ?>
+    <?php if (!empty($this->task->configModel->get('matomo_admin_url'))): ?>
+        <a href="<?= $this->task->configModel->get('matomo_admin_url', '') ?>" class="btn" rel="noreferrer noopener" target="_blank" title="<?= t('Opens in a new window') ?>">
+            <img src="/plugins/MatomoConnector/Assets/matomo-icon.svg" alt="<?= t('Matomo icon') ?>"> <?= t('Matomo Admin Login') ?>
+        </a>
+    <?php endif ?>
+    <a href="https://matomo.org/guides" class="btn" rel="noreferrer noopener" target="_blank" title="<?= t('Opens in a new window') ?>">
+        <img src="/plugins/MatomoConnector/Assets/matomo-icon.svg" alt="<?= t('Matomo icon') ?>"> <?= t('Knowledge Base') ?>
+    </a>
+    <a href="https://forum.matomo.org" class="btn" rel="noreferrer noopener" target="_blank" title="<?= t('Opens in a new window') ?>">
+        <img src="/plugins/MatomoConnector/Assets/matomo-icon.svg" alt="<?= t('Matomo icon') ?>"> <?= t('Community Forum') ?>
+    </a>
+    </div>
 </div>
