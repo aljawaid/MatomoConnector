6d57ee5f0f58c949baade0139011f095f8653f5f
aljawaid <jawaid_imran@hotmail.com>
2022-12-05 00:04:01 +0000

    Add Tracking Code

--- Template/layout/head.php
+++ Template/layout/head.php
@@ -2,10 +2,29 @@
 $website_page_name_js = str_replace(' ', '+', $this->task->configModel->get('website_page_name', ''))
 ?>
 
-<!-- MATOMO CODE -->
-<!-- END OF MATOMO CODE -->
-<!-- Matomo -->
-<!-- End Matomo Code -->
+<?php if (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id'))): ?>
+    <!-- MATOMO CODE -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        <?php if ($this->task->configModel->get('doc_title', '') == 'prepend'): ?>
+        _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+        <?php endif ?>
+        <?php if (!empty($this->task->configModel->get('domain'))): ?>
+        _paq.push(["setCookieDomain", "*.<?= $this->task->configModel->get('domain', '') ?>"]);
+        <?php endif ?>
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="<?= $this->task->configModel->get('matomo_admin_url', '') ?>";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '<?= $this->task->configModel->get('site_id', '') ?>']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- END OF MATOMO CODE -->
+<?php endif ?>
 
 <?php if ($this->task->configModel->get('image_tracking', '') == 'enable_image_tracking_js'): ?>
     <!-- MATOMO IMAGE TRACKER -->
