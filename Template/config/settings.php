396a06cf022483d9be8869d6b5f6202a1d883d14
aljawaid <jawaid_imran@hotmail.com>
2022-12-06 08:04:48 +0000

    - Add CSS Class - Add Apache Version Section
    - no calculations in front end yet

--- Template/config/settings.php
+++ Template/config/settings.php
@@ -58,7 +58,7 @@
             <div class="">
                 <ul class="">
                     <li class="">
-                        <?= $this->form->radio('apache_version', t('Use Apache version'), 'cloak', isset($values['apache_version']) && $values['apache_version'] == 'cloak') ?>
+                        <?= $this->form->radio('apache_version', t('Use Apache version'), 'cloak', false, 'follow-instructions', isset($values['apache_version']) && $values['apache_version'] == 'cloak') ?>
                     </li>
                     <li class="">
                         <?= $this->form->radio('apache_version', t('Don\'t use Apache version'), 'dont_cloak', true, isset($values['apache_version']) && $values['apache_version'] == 'dont_cloak') ?>
