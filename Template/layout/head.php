65f7eb25de3384ff53f747e13f8ca1e85b198380
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-07 02:01:25 +0000

    NEW: Add Cloak Version Script to Image Tracking as JS Fallback

--- Template/layout/head.php
+++ Template/layout/head.php
@@ -26,12 +26,25 @@ $website_page_name_js = str_replace(' ', '+', $this->task->configModel->get('web
     <!-- END OF MATOMO CODE -->
 <?php endif ?>
 
+<?php //// IF 'ENABLE IMAGE TRACKING AS JS FALLBACK' //// ?>
 <?php if ($this->task->configModel->get('image_tracking', '') == 'enable_image_tracking_js'): ?>
     <!-- MATOMO IMAGE TRACKER -->
-    <?php if (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('website_page_name'))): ?>
+    <?php //// ELSE IF 'ADMIN URL' & 'SITE ID' & 'CLOAK' //// ?>
+    <?php if (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('website_page_name')) && ($this->task->configModel->get('apache_version', '') == 'cloak')): ?>
+        <noscript><p><img
+        referrerpolicy="no-referrer-when-downgrade"
+        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;rainbow&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name_js ?>" style="border:0" alt="" /></p></noscript>
+    <?php //// IF 'ADMIN URL' & 'SITE ID' & 'PAGE NAME' & 'CLOAK' //// ?>
+    <?php elseif (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('apache_version', '') == 'cloak')): ?>
+        <noscript><p><img
+        referrerpolicy="no-referrer-when-downgrade"
+        src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;rainbow&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name_js ?>" style="border:0" alt="" /></p></noscript>
+    <?php //// ELSE IF 'ADMIN URL' & 'SITE ID' & 'PAGE NAME' //// ?>
+    <?php elseif (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id')) && ($this->task->configModel->get('website_page_name'))): ?>
         <noscript><p><img
         referrerpolicy="no-referrer-when-downgrade"
         src="<?= $this->task->configModel->get('matomo_admin_url', '') ?>&#47;matomo.php&#63;idsite=<?= $this->task->configModel->get('site_id', '') ?>&amp;rec=1&amp;action_name=<?= $website_page_name_js ?>" style="border:0" alt="" /></p></noscript>
+    <?php //// ELSE IF 'ADMIN URL' & 'SITE ID' //// ?>
     <?php elseif (!empty($this->task->configModel->get('matomo_admin_url')) && ($this->task->configModel->get('site_id'))): ?>
         <noscript><p><img
         referrerpolicy="no-referrer-when-downgrade"
