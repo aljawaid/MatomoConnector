c88e6dee586632efc4654c61feec8f6e9ac87351
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-26 21:11:44 +0000

    FIX: Unix (LF) Line Endings and Tabs > Spaces

--- Locale/translations-starter-template.php
+++ Locale/translations-starter-template.php
@@ -8,66 +8,67 @@
 <!-- ------------------- -->
 <!-- REMOVE THIS SECTION -->
 <?php
+
 return array(
-  //
-  // GENERAL
-  //
-  'Connect your Kanboard instance to Matomo Analytics to track visitors to your site. Set the standard tracking code or use the optional JavaScript fallback code with further options for image tracking.' => '',
-  'Settings' => '',
-  'Settings saved successfully.' => '',
-  'Unable to save your settings.' => '',
-  //
-  // config/integrations.php
-  //
-  'Connector Installed' => '',
-  'Configured' => '',
-  'Not Configured' => '',
-  //
-  // config/settings.php
-  //
-  'Matomo logo' => '',
-  'Admin Interface' => '',
-  'Website Name' => '',
-  'My Kanboard Instance' => '',
-  'My Matomo Admin' => '',
-  'This label is used for the login button or leave blank for "Matomo Admin Login"' => '',
-  'Matomo Admin Interface' => '',
-  'Enter the complete URL to your Matomo interface with the trailing slash (or without the trailing slash if using the Apache version)' => '',
-  'Opens in a new window' => '',
-  'Matomo icon' => '',
-  'Matomo Admin Login' => '',
-  'Knowledge Base' => '',
-  'Community Forum' => '',
-  'Tracking Code' => '',
-  'Site ID' => '',
-  'Bad requests in the browser console are usually the result of an incorrect Site ID' => '',
-  'Track Across All Subdomains' => '',
-  'Enter your domain' => '',
-  'Your Kanboard domain is detected as' => '',
-  'Prepend the Kanboard site domain to the page title when tracking' => '',
-  'Don\'t prepend' => '',
-  'Fallback Code' => '',
-  'Image Tracking Code' => '',
-  'Optional' => '',
-  'Enable Image Tracking' => '',
-  'Enable Image Tracking as JavaScript Fallback' => '',
-  'Disable Image Tracking' => '',
-  'When using this option as a fallback for JavaScript tracking, the same code will be surrounded in' => '',
-  'HTML tags' => '',
-  'Page Name' => '',
-  'My Kanboard Instance' => '',
-  'This is the title of the action being tracked' => '',
-  'Save Settings' => '',
-  'Connector' => '',
-  'Back to Integrations' => '',
-  'Apache Version' => '',
-  'Use Apache version' => '',
-  'Don\'t use Apache version' => '',
-  'Instructions for Using Apache Version' => '',
-  'Bypass Adblockers Using Apache' => '',
-  'Cloaking or masking is a safe way to avoid detection by the filter lists used by Adblockers. It is up to you to be data-privacy compliant with your local regulations directly through' => '',
-  'Apache module must be enabled on the server where your Matomo is installed.' => '',
-  'On your Matomo server, create or update your' => '',
-  'to include the following code:' => '',
-  'Source:' => '',
+    //
+    // GENERAL
+    //
+    'Connect your Kanboard instance to Matomo Analytics to track visitors to your site. Set the standard tracking code or use the optional JavaScript fallback code with further options for image tracking.' => '',
+    'Settings' => '',
+    'Settings saved successfully.' => '',
+    'Unable to save your settings.' => '',
+    //
+    // config/integrations.php
+    //
+    'Connector Installed' => '',
+    'Configured' => '',
+    'Not Configured' => '',
+    //
+    // config/settings.php
+    //
+    'Matomo logo' => '',
+    'Admin Interface' => '',
+    'Website Name' => '',
+    'My Kanboard Instance' => '',
+    'My Matomo Admin' => '',
+    'This label is used for the login button or leave blank for "Matomo Admin Login"' => '',
+    'Matomo Admin Interface' => '',
+    'Enter the complete URL to your Matomo interface with the trailing slash (or without the trailing slash if using the Apache version)' => '',
+    'Opens in a new window' => '',
+    'Matomo icon' => '',
+    'Matomo Admin Login' => '',
+    'Knowledge Base' => '',
+    'Community Forum' => '',
+    'Tracking Code' => '',
+    'Site ID' => '',
+    'Bad requests in the browser console are usually the result of an incorrect Site ID' => '',
+    'Track Across All Subdomains' => '',
+    'Enter your domain' => '',
+    'Your Kanboard domain is detected as' => '',
+    'Prepend the Kanboard site domain to the page title when tracking' => '',
+    'Don\'t prepend' => '',
+    'Fallback Code' => '',
+    'Image Tracking Code' => '',
+    'Optional' => '',
+    'Enable Image Tracking' => '',
+    'Enable Image Tracking as JavaScript Fallback' => '',
+    'Disable Image Tracking' => '',
+    'When using this option as a fallback for JavaScript tracking, the same code will be surrounded in' => '',
+    'HTML tags' => '',
+    'Page Name' => '',
+    'My Kanboard Instance' => '',
+    'This is the title of the action being tracked' => '',
+    'Save Settings' => '',
+    'Connector' => '',
+    'Back to Integrations' => '',
+    'Apache Version' => '',
+    'Use Apache version' => '',
+    'Don\'t use Apache version' => '',
+    'Instructions for Using Apache Version' => '',
+    'Bypass Adblockers Using Apache' => '',
+    'Cloaking or masking is a safe way to avoid detection by the filter lists used by Adblockers. It is up to you to be data-privacy compliant with your local regulations directly through' => '',
+    'Apache module must be enabled on the server where your Matomo is installed.' => '',
+    'On your Matomo server, create or update your' => '',
+    'to include the following code:' => '',
+    'Source:' => '',
 );
